60d72a18627b6f10965b6b2b90d0e1edd129e381
satelit0 <satelite0ster@gmail.com>
2023-04-12 13:10:09 -0400

    log

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || 3002;
+  const PORT = process.env.PORT || 3000;
   console.log('Puerto ======>', PORT);
   
   await app.listen(PORT);
